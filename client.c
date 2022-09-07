6c7a19fdc57b6408eb7f889c47602f18df177374
Andre Christofori <andre.ceco@gmail.com>
2022-09-07 22:12:31 +0100

    Check Norminette

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: andchris <andchris1987@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 21:57:15 by andchris          #+#    #+#             */
-/*   Updated: 2022/08/30 21:25:10 by andchris         ###   ########.fr       */
+/*   Updated: 2022/09/07 22:11:25 by andchris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void    send_bit(int pid, char *str, size_t len)
             else
                 kill(pid, SIGUSR1);
             shift++;
-            usleep(150);
+            usleep(100);
         }
         i++;
     }
